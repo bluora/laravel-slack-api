a13c5fd2ac2fd6e766feb0eebbd0dbfce7e3b2a7
Rocco Howard <rocco@hnh.digital>
2017-06-20 23:45:31 +0000

    Reorganised templates and build out models correctly

--- bin/build.php
+++ bin/build.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-$src_dir = __DIR__.'/../vendor/cleentfaar/slack/src/CL/Slack';
+$src_dir = __DIR__.'/../vendor/bluora/php-slack-api/src/CL/Slack';
 
 if (!file_exists($src_dir)) {
     echo "`$src_dir` does not exist.\n\nRun `composer install`.\n\n";
@@ -17,13 +17,16 @@ exec('rm -rf '.__DIR__.'/../src/Model');
 mkdir(__DIR__.'/../src/Payload', $mode, true);
 mkdir(__DIR__.'/../src/Model', $mode, true);
 
+$class_template = file_get_contents(__DIR__.'/src/class.template');
+$method_template = file_get_contents(__DIR__.'/src/method.template');
+
 foreach (['Payload', 'Model'] as $class_type) {
     $working_path = $src_dir.'/'.$class_type;
     $files = scandir($working_path);
 
-    $class_template = file_get_contents(__DIR__.'/src/class.template');
-    $class_contents_template = file_get_contents(__DIR__.'/src/'.strtolower($class_type).'.class.template');
-    $method_template = file_get_contents(__DIR__.'/src/'.strtolower($class_type).'.method.template');
+    if ($class_type == 'Payload') {
+        $class_contents_template = file_get_contents(__DIR__.'/src/'.strtolower($class_type).'.class.template');
+    }
 
     foreach ($files as $path) {
         if ($path == '.' || $path == '..') {
@@ -35,16 +38,16 @@ foreach (['Payload', 'Model'] as $class_type) {
         }
 
         $original_class_name = pathinfo($path, PATHINFO_FILENAME);
-        $class_aliases = "use CL\\Slack\\Payload\\$original_class_name as Original$original_class_name;\n";
+        $class_aliases = "use CL\\Slack\\$class_type\\$original_class_name as Original$original_class_name;\n";
         $class_methods = '';
         $class_contents = '';
 
-        if (stripos($path, 'PayloadResponse') === false) {
+        if (stripos($path, 'PayloadResponse') === false || $class_type == 'Model') {
 
-            $class_contents = file_get_contents($working_path.'/'.$path);
+            $current_class_contents = file_get_contents($working_path.'/'.$path);
 
             $matches= [];
-            $methods = preg_match_all("/public function set(.*?)\(/im", $class_contents, $matches);
+            $methods = preg_match_all("/public function set(.*?)\(/im", $current_class_contents, $matches);
 
             foreach ($matches[1] as $method_name) {
                 $new_method_name = lcfirst($method_name);
@@ -54,10 +57,12 @@ foreach (['Payload', 'Model'] as $class_type) {
                 $class_methods .= str_replace(['{$method_name}', '{$method_argument}', '{$new_method_name}', '{$text_method_name}'], [$method_name, $method_argument, $new_method_name, $text_method_name], $method_template);
             }
 
-            $class_contents = str_replace(['{$class_type}', '{$class_name}', '{$class_methods}'], [$class_type, $original_class_name, $class_methods], $class_contents_template);
+            $class_contents .= $class_methods;
+            $class_contents .= str_replace(['{$class_type}', '{$class_name}'], [$class_type, $original_class_name], $class_contents_template);
 
-            $class_aliases .= "use CL\\Slack\\Payload\\${original_class_name}Response;\n";
-            $class_aliases .= "use CL\\Slack\\Transport\\ApiClient;\n";
+            if ($class_type == 'Payload') {
+                $class_aliases .= "use CL\\Slack\\$class_type\\${original_class_name}Response;\n";
+            }
         }
 
         $file_contents = str_replace(['{$class_aliases}', '{$class_type}', '{$class_name}', '{$class_contents}'], [$class_aliases, $class_type, $original_class_name, $class_contents], $class_template);
